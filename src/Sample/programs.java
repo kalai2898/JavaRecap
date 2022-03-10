98512da56b6400cf1b0a31486a34db7f8c062257
kalai2898 <nkalai2898@gmail.com>
2022-03-10 10:31:56 +0530

    First commit

---
+++ src/Sample/programs.java
@@ -0,0 +1,24 @@
+package Sample;
+
+public class programs {
+
+	public static void main(String[] args) {
+		 int sum=0;
+		int a[]= {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		int size =a.length;
+	   for(int i=0;i<size;i++)
+	   {
+		  
+		   sum=sum+a[i];
+	   }
+	   System.out.println(sum);
+ int  b[]= {11, 12, 13, 14, 15, 16};
+ for(int j=0;j<b.length;j++)
+ {
+	 System.out.println(b[j]);
+ }
+
+
+	}
+
+}
